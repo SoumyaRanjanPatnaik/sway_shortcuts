2f745a5d6904ec3e63e283235dd2cd533be25ab4
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-04-03 14:28:56 +0530

    Basic App Finished

--- dialog.h
+++ dialog.h
@@ -3,9 +3,14 @@
 
 #include <QDialog>
 #include <QFile>
+#include <QVector>
 #include <QProcessEnvironment>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QKeyEvent>
+#include <QKeySequenceEdit>
 
 
 QT_BEGIN_NAMESPACE
@@ -19,22 +24,51 @@ class Dialog : public QDialog
 public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
+    void setApplication(QApplication *a){
+        this->a = a;
+    };
 
 private:
     // Private Variables
     Ui::Dialog *ui;
+    QApplication *a=nullptr;
+
+    // Locations and Files
     QString configDir;
     QString configFilename = "config.json";
     QString configLoc;
     QFile configFile;
+
+    // Json object storing keyboard config
     QJsonDocument kbdConfig;
 
-    // Private Functions
+    // I/O Config
+    void configureLocations();
     void readJson();
     void writeJson();
-    void writeJson(QByteArray json);
+    void writeJson(QByteArray &json);
+    void writeConfig(QByteArray &s);
+
+    // Helper Functions
+    bool validateConfigObject(QJsonObject &obj);
+    void populateCmb();
     void resetDefaults();
+    QByteArray generateConfig();
+    void clearKsShortcut();
+    QString getKbdShortcut();
+    void setKbdShortcut(QString combi);
+
+
 private slots:
     void on_pbReset_clicked();
+    void on_pbSave_clicked();
+    void on_cmbAction_currentIndexChanged(int index);
+
+    void on_pbRecord_clicked();
+//    void saveSequence();
+    void on_leShortcut_textChanged(const QString &arg1);
+
+signals:
+    void save();
 };
 #endif // DIALOG_H
